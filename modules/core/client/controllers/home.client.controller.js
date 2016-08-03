6788800aa3ca311deb9d010a587b11c8b877c800
Atehortua <wyatehortua@gmail.com>
2016-08-03 15:44:46 -0500

    configuraciones basicas para trabajar con material

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$mdSidenav', '$mdBottomSheet', '$mdDialog','$mdToast','$timeout','$mdMedia',
+  function($scope, Authentication,  $mdSidenav, $mdBottomSheet, $mdDialog, $mdToast, $timeout, $mdMedia) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
   }
